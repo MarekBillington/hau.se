09c944fc3ddbc286dcdd79cebf13f2308a7d08ba
merak <MarekBillington@users.noreply.github.com>
2023-01-11 17:42:30 +0000

    House Additions
    Created House Structs for existing and new objects
    
    Added post endpoint for houses

--- hause-be/src/house.rs
+++ hause-be/src/house.rs
@@ -1,10 +1,38 @@
+use rocket::serde::{Serialize, Deserialize, json::Json};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct House {
+    id: i64,
+    address: String,  
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct NewHouse {
+    address: String,
+}
+
 #[rocket::get("/house")]
-pub fn all_houses() -> &'static str {
-    "all houses"
+pub fn all_houses() -> Json<Vec<House>> {
+    let h = House {id: 1, address: String::from("abc")};
+    let h2 = House {id: 2, address: String::from("abc")};
+
+    let mut vec = Vec::new();
+    vec.push(h);
+    vec.push(h2);
+    
+    Json(vec)
 }
 
 #[rocket::get("/house/<id>")]
-pub fn house_by_id(id: i64) -> String {
-    let output: String = id.to_string();
-    output
+pub fn house_by_id(id: i64) -> Json<House> {
+    let h3 = House {id: 3, address: String::from("123 test road")};
+    Json(h3)
+}
+
+#[rocket::post("/house", format = "application/json", data = "<house>")]
+pub fn create_house(house: Json<NewHouse>) -> Json<House> {
+    let h = House {id: 1, address: String::from(&house.address)};
+    Json(h)
 }
--- hause-be/src/main.rs
+++ hause-be/src/main.rs
@@ -6,7 +6,8 @@ mod house;
 fn rocket() -> _ {
     let routes:Vec<rocket::Route> = routes![
         house::all_houses,
-        house::house_by_id
+        house::house_by_id,
+        house::create_house,
     ];
     
     // @todo some kind of security fairing required on requests
