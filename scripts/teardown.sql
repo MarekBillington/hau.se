bf654e4272003c0fc2178c5d9010e05c9cd2ade8
merak <MarekBillington@users.noreply.github.com>
2023-03-14 23:58:42 +0000

    Teardown sql for when test data goes too far
    Burn it all down!

---
+++ scripts/teardown.sql
@@ -0,0 +1,12 @@
+
+-- Must be in order for FK to drop correctly
+
+drop table "houses";
+drop table "user_links";
+drop table "portfolios";
+drop table "users";
+
+
+-- Do Not Drop:
+--    countries
+--    user_roles
